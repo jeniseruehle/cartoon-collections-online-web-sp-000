3f081aff6e04169887bb4a062f5aef4bda78de13
Jenise Ruehle <jela07@gmail.com>
2020-01-31 18:23:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index.map { |dwarf, index| puts " #{index}. #{dwarf}" }.join
+  array.each_with_index.map { |dwarf, index| puts " #{index+1}. #{dwarf}" }.join
 end
 
 def summon_captain_planet# code an argument here
