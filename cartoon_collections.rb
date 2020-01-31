eb0091c46e2b3aa6d6fff761cda763f07f2be73d
Jenise Ruehle <jela07@gmail.com>
2020-01-31 18:27:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! { |x| + 
+  planeteer_calls.map! { |x| + "!" }
 end
 
 def long_planeteer_calls# code an argument here
