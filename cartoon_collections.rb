6b11168f3e360c3323e936dbe55689dab74c39d7
Jenise Ruehle <jela07@gmail.com>
2020-01-31 18:57:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? { |x| x.length > 4 }
 end
 
-def find_the_cheese(strings)
-  # the array below is here to help
+def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
