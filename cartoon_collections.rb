1ce197b24591ac2c0f8a8af6e6dd9f7c8110a53a
Jenise Ruehle <jela07@gmail.com>
2020-01-31 18:31:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! { |x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 
